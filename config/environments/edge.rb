91272205fbfab8785bc915d743fb0b0d5228e613
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2025-10-22 08:41:15 -0500

    Fix database configuration for single-database Heroku deployment

--- config/environments/edge.rb
+++ config/environments/edge.rb
@@ -52,7 +52,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Raise email delivery errors for edge testing
   config.action_mailer.raise_delivery_errors = true
