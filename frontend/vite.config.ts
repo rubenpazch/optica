bb0f4608795cc0125199a8f0df413b6e0749a981
Ruben Paz Chuspe <ruben.paz@bairesdev.com>
2025-10-23 16:14:21 -0500

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -15,7 +16,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../public/dist',
+    outDir: '../public/assets',
     emptyOutDir: true,
     rollupOptions: {
       output: {
